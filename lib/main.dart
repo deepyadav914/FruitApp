5820ecc6063d55c6464c07349154914a7bfcb641
deep yadav <deepbca.169@gmail.com>
2024-09-18 10:39:37 +0530

    18/09
    done

---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:fruit_app/views/splash_screen.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+
+      debugShowCheckedModeBanner: false,
+
+      home: AnimatedSplashScreen()
+    );
+  }
+}
+
+
---
+++ lib/views/splash_screen.dart
@@ -0,0 +1,63 @@
+import 'dart:async';
+import 'package:flutter/material.dart';
+import 'onboarding_screen.dart';
+
+
+class AnimatedSplashScreen extends StatefulWidget {
+  @override
+  _AnimatedSplashScreenState createState() => _AnimatedSplashScreenState();
+}
+
+class _AnimatedSplashScreenState extends State<AnimatedSplashScreen>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+  late Animation<double> _animation;
+
+  get assets => null;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(
+      vsync: this,
+      duration: Duration(seconds: 2),
+    );
+
+    _animation = CurvedAnimation(
+      parent: _controller,
+      curve: Curves.easeInOut,
+    );
+
+    _controller.forward();
+
+    Timer(Duration(seconds: 4), () {
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(builder: (context) => OnboardingScreen()),
+      );
+    });
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+
+    return Scaffold(
+      backgroundColor: Colors.green,
+      body: Center(
+        child: ScaleTransition(
+          scale: _animation,
+          child: Image.asset("assets/splash.png")),
+        ),
+
+    );
+  }
+}
+
+
+
